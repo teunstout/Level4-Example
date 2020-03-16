f6c5e43cd2ca5b3de8f36ef10e8d90da9a99351f
TeunStout <teunstout@hotmail.com>
2020-03-16 23:50:31 +0100

    2.2 correct

--- RemindersApp/app/src/main/java/com/example/remindersapp/database/ReminderRepository.kt
+++ RemindersApp/app/src/main/java/com/example/remindersapp/database/ReminderRepository.kt
@@ -5,28 +5,27 @@ import com.example.remindersapp.Reminder
 
 class ReminderRepository(context: Context) {
 
-    private lateinit var reminderDao: ReminderDOA
+    private var reminderDao: ReminderDOA
 
     init {
         val reminderRoomDatabase = ReminderRoomDatabase.getDatabase(context)
-        if (reminderRoomDatabase != null) {
-            reminderDao = reminderRoomDatabase.reminderDao()
-
-            fun getAllReminders(): List<Reminder> {
-                return reminderDao.getAllReminders()
-            }
-
-            fun insertReminder(reminder: Reminder) {
-                reminderDao.insertReminder(reminder)
-            }
-
-            fun deleteReminder(reminder: Reminder) {
-                reminderDao.deleteReminder(reminder)
-            }
-
-            fun updateReminder(reminder: Reminder) {
-                reminderDao.updateReminder(reminder)
-            }
-        }
+        reminderDao = reminderRoomDatabase!!.reminderDao()
     }
+
+    fun getAllReminders(): List<Reminder> {
+        return reminderDao.getAllReminders()
+    }
+
+    fun insertReminder(reminder: Reminder) {
+        reminderDao.insertReminder(reminder)
+    }
+
+    fun deleteReminder(reminder: Reminder) {
+        reminderDao.deleteReminder(reminder)
+    }
+
+    fun updateReminder(reminder: Reminder) {
+        reminderDao.updateReminder(reminder)
+    }
+
 }
